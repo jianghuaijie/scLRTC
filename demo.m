23a2d23ba115e942b83e93b82d4cd0eb9e583dc4
jianghuaijie <30695578+jianghuaijie@users.noreply.github.com>
2021-02-19 21:44:48 +0800

    Add files via upload

--- demo.m
+++ demo.m
@@ -2,8 +2,8 @@ M = readtable('YANDATA.csv','Delimiter',',','ReadRowNames',1,'ReadVariableNames'
 M0 = table2array(M);
 k=5;
 p=5;
-rho=1e-4;
-epsilon=1e-3;
+rho=1e-5;
+epsilon=1e-2;
 alpha= [1,1e-2,2e-3];
 rebuild =scLRTC(M0,k,p,rho,epsilon,alpha);
 csvwrite('yanltrc.csv',rebuild);
