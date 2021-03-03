802f75c8d86ed4d6de15a45d9567b4fac9cba0ba
jianghuaijie <30695578+jianghuaijie@users.noreply.github.com>
2021-03-03 15:14:53 +0800

    Add files via upload

--- demo.m
+++ demo.m
@@ -1,10 +1,23 @@
-M = readtable('YANDATA.csv','Delimiter',',','ReadRowNames',1,'ReadVariableNames',1);
+clc;
+clear;
+close all;
+input_path='YANDATA.csv'; 
+%the dir of input file
+M = readtable(input_path,'Delimiter',',','ReadRowNames',1,'ReadVariableNames',1);
 M0 = table2array(M);
 k=5;
+%Construct the size of the third-order low-rank tensor
 p=5;
+%Construct the size of the third-order low-rank tensor
 rho=1e-5;
+%the initial value of the parameter; it should be small enough
 epsilon=1e-2;
+%the tolerance of the relative difference of outputs of two neighbor iterations
 alpha= [1,1e-2,2e-3];
+%the coefficient of the objective function, i.e., \|X\|_* := \alpha_i \|X_{i(i)}\|_*
 rebuild =scLRTC(M0,k,p,rho,epsilon,alpha);
-csvwrite('yanltrc.csv',rebuild);
-fprintf('complete!');
+output_path ='yanltrc.csv';
+%the dir of output file
+csvwrite(output_path,rebuild);
+%save the imputation result
+fprint('complete!');
